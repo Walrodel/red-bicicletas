58c3243600eda81309364bc58e98f6bfd1004dc9
walrodel <maquetador6@booksandbooksdigital.com.co>
2020-07-02 15:57:45 -0500

    Crud Bicicletas

---
+++ models/bicicleta.js
@@ -0,0 +1,41 @@
+var Bicicleta = function (id, color, modelo, ubicacion) {
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+// Bicicleta.prototype.toString = () => {
+//     return `id: ${this.id} color: ${this.color}`;
+// }
+
+Bicicleta.allBicis = [];
+
+Bicicleta.add = (aBici) => {
+    Bicicleta.allBicis.push(aBici);
+}
+
+Bicicleta.findById = (aBiciId) => {
+    let bici = Bicicleta.allBicis.find(x => x.id.toString() === aBiciId);
+    if(bici)
+        return bici;
+    else
+        throw new Error(`No existe la bicicleta con id: ${aBiciId}`)
+}
+
+Bicicleta.removeById = (aBiciId) => {
+    Bicicleta.allBicis.forEach((bici, index) => {
+        if(bici.id === aBiciId){
+            Bicicleta.allBicis.splice(index, 1);
+            return;
+        }
+    });
+}
+
+var a = new Bicicleta(1, 'rojo', 'urbana', [4.7481775, -74.0974518]);
+var b = new Bicicleta(2, 'blaca', 'urbana', [4.7440077, -74.0974715]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
---
+++ routes/bicicletas.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/Bicicleta');
+
+router.get('/', controller.bicicleta_list);
+router.get('/create', controller.bicicleta_create_get);
+router.post('/create', controller.bicicleta_create_post);
+router.get('/:id/update', controller.bicicleta_update_get);
+router.post('/:id/update', controller.bicicleta_update_post);
+router.post('/:id/delete', controller.bicicleta_delete_post);
+
+module.exports = router;
